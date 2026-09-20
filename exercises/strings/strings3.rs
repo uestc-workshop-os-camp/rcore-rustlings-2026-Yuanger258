7ed88be9481538dbb11db0a252e3b668d34dfd22
Yuanger258 <3278693507qq.com>
2026-09-20 20:25:20 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,25 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
+    
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut str_add = String::from(input);
+    str_add.push_str(" world!");
+    str_add
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let newstring = input.replace("cars","balloons");
+    newstring
+    
 }
 
 #[cfg(test)]
